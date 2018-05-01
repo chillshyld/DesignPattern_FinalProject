595f008fc223810240bda2fc241a217a8b5c7e19
Chieh <chillshyld@gmail.com>
2018-05-01 23:37:57 +0700

    modify TabDelimitedMovieFinder by adding tokenizer to tokenize string from movies.txt

--- src/spring/TabDelimitedMovieFinder.java
+++ src/spring/TabDelimitedMovieFinder.java
@@ -5,6 +5,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class TabDelimitedMovieFinder implements MovieFinder{
     public String inputFile;
@@ -16,11 +17,19 @@ public class TabDelimitedMovieFinder implements MovieFinder{
     public ArrayList<Movies> readMoviesFile() {
         ArrayList<Movies> moviesList = new ArrayList<Movies>();
         Path inputPath = Paths.get(this.inputFile);
+
         try (Scanner scanner = new Scanner(inputPath)) {
             String moviesTitle;
+            String moviesDesription;
+            String moviesReleaseDate;
+            String line;
             while (scanner.hasNextLine()){
-                moviesTitle = scanner.nextLine();
-                moviesList.add(new Movies(moviesTitle,"",""));
+                line = scanner.nextLine();
+                StringTokenizer tokenizer = new StringTokenizer(line,",");
+                moviesTitle = tokenizer.nextToken();
+                moviesDesription = tokenizer.nextToken();
+                moviesReleaseDate = tokenizer.nextToken();
+                moviesList.add(new Movies(moviesTitle,moviesDesription,moviesReleaseDate));
             }
         }catch (IOException err){
             System.out.println("Error reading file: " + err);
